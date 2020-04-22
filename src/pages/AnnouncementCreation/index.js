18feacafcbf66b3f30a34e2cb7f12285e76dd5ef
JM Fantin <joao.fantin@acad.pucrs.br>
2020-04-22 00:16:00 -0300

    CreateAnnouncement - breed selects appear on category selections

--- src/pages/AnnouncementCreation/index.js
+++ src/pages/AnnouncementCreation/index.js
@@ -15,7 +15,9 @@ import { Container, UploadVideo } from "./styles";
 
 export default function AnnouncementCreation() {
   const [primary, setPrimary] = useState("");
+  const [primaryBreed, setPrimaryBreed] = useState("");
   const [secondary, setSecondary] = useState("");
+  const [secondaryBreed, setSecondaryBreed] = useState("");
   const [secondaryToggle, setSecondaryToggle] = useState(false);
 
   const primaryOptions = [
@@ -29,9 +31,8 @@ export default function AnnouncementCreation() {
   ];
 
   let secondaryOptions = [];
-
-  let primaryVariants = null;
-  let secondaryVariants = null;
+  let primaryVariants,
+    secondaryVariants = [];
 
   /* Chernobyl : Do you know what you're doing? Well, I didn't. */
   if (primary === "novilho") {
@@ -67,9 +68,31 @@ export default function AnnouncementCreation() {
     secondaryOptions = [];
   }
 
+  const breeds = [
+    { label: "ANGUS (ABERDEEN)", value: "aberdeen angus" },
+    { label: "ANGUS (RED)", value: "red angus" },
+    { label: "BRAFORD", value: "braford" },
+    { label: "BRANGUS", value: "brangus" },
+    { label: "BRITÂNICOS", value: "britânicos" },
+    { label: "CANCHIN", value: "canchin" },
+    { label: "CHAROLES", value: "charoles" },
+    { label: "CRUZAS EUROPEIAS", value: "cruzas europeias" },
+    { label: "CRUZAS LEITEIRAS", value: "cruzas leiteiras" },
+    { label: "CRUZAS ZEBU", value: "cruzas zebu" },
+    { label: "DEVON", value: "devon" },
+    { label: "EUROPEUS", value: "europeus" },
+    { label: "HEREFORD", value: "hereford" },
+    { label: "LIMOUSIN", value: "limousin" },
+    { label: "NELORE", value: "nelore" },
+    { label: "SIMENTAL", value: "simental" },
+    { label: "TABAPUÃ", value: "tabapuã" },
+    { label: "ZEBU", value: "zebu" },
+  ];
+
   const handleSelection = (value) => {
     setPrimary(value);
     setSecondary("");
+    setSecondaryBreed("");
     if (value === "touro" || value === "vaca invernar" || value === null) {
       setSecondaryToggle(false);
     } else {
@@ -92,7 +115,7 @@ export default function AnnouncementCreation() {
       </TouchableOpacity>
 
       {/* Primary Category Selection */}
-      <Text>Selecione uma categoria</Text>
+      <Text>Indique uma categoria para o gado que será vendido</Text>
       <RNPickerSelect
         placeholder={{
           label: "———",
@@ -102,6 +125,20 @@ export default function AnnouncementCreation() {
         onValueChange={(value) => handleSelection(value)}
         items={primaryOptions}
       />
+      {primary ? (
+        <>
+          <Text>Selecione a raça correspondente à categoria</Text>
+          <RNPickerSelect
+            placeholder={{
+              label: "———",
+              value: null,
+              color: general.styles.colors.light,
+            }}
+            onValueChange={(value) => setPrimaryBreed(value)}
+            items={breeds}
+          />
+        </>
+      ) : null}
 
       {/* Secondary Category Selection */}
       {secondaryToggle ? (
@@ -116,6 +153,20 @@ export default function AnnouncementCreation() {
             onValueChange={(value) => setSecondary(value)}
             items={secondaryOptions}
           />
+          {secondary ? (
+            <>
+              <Text>Selecione a raça correspondente à segunda categoria</Text>
+              <RNPickerSelect
+                placeholder={{
+                  label: "———",
+                  value: null,
+                  color: general.styles.colors.light,
+                }}
+                onValueChange={(value) => setSecondaryBreed(value)}
+                items={breeds}
+              />
+            </>
+          ) : null}
         </>
       ) : null}
     </Container>
