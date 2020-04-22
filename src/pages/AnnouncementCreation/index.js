4908d1d49b3ceea19f4ef58c97a41e42ed829cde
JM Fantin <joao.fantin@acad.pucrs.br>
2020-04-21 23:38:40 -0300

    CreateAnnouncement - match multiple categories

--- src/pages/AnnouncementCreation/index.js
+++ src/pages/AnnouncementCreation/index.js
@@ -2,8 +2,8 @@
     - We are not using styled.Picker since react-native/picker is deprecated.
     - We are also not using react-native-community/picker because it sucks.
 */
-import React from "react";
-import { TouchableOpacity, Text } from "react-native";
+import React, { useState } from "react";
+import { TouchableOpacity, Text, StyleSheet } from "react-native";
 import PropTypes from "prop-types";
 
 import RNPickerSelect from "react-native-picker-select";
@@ -13,7 +13,70 @@ import { Ionicons } from "@expo/vector-icons";
 
 import { Container, UploadVideo } from "./styles";
 
-export default function AnnouncementCreation(props) {
+export default function AnnouncementCreation() {
+  const [primary, setPrimary] = useState("");
+  const [secondary, setSecondary] = useState("");
+  const [secondaryToggle, setSecondaryToggle] = useState(false);
+
+  const primaryOptions = [
+    { label: "NOVILHO", value: "novilho" },
+    { label: "TERNEIRO", value: "terneiro" },
+    { label: "TOURO", value: "touro" },
+    { label: "TERNEIRA", value: "terneira" },
+    { label: "NOVILHA", value: "novilha" },
+    { label: "VACA", value: "vaca" },
+    { label: "VACA INVERNAR", value: "vaca invernar" },
+  ];
+
+  let secondaryOptions = [];
+
+  let primaryVariants = null;
+  let secondaryVariants = null;
+
+  /* Chernobyl : Do you know what you're doing? Well, I didn't. */
+  if (primary === "novilho") {
+    secondaryOptions = [
+      { label: "TERNEIRO", value: "terneiro" },
+      { label: "NOVILHA", value: "novilha" },
+      { label: "VACA", value: "vaca" },
+    ];
+  } else if (primary === "terneiro") {
+    secondaryOptions = [
+      { label: "NOVILHO", value: "novilho" },
+      { label: "TERNEIRA", value: "terneira" },
+    ];
+  } else if (primary === "terneira") {
+    secondaryOptions = [
+      { label: "NOVILHA", value: "novilha" },
+      { label: "TERNEIRO", value: "terneiro" },
+    ];
+  } else if (primary === "novilha") {
+    secondaryOptions = [
+      { label: "TERNEIRA", value: "terneira" },
+      { label: "NOVILHO", value: "novilho" },
+      { label: "VACA", value: "vaca" },
+    ];
+  } else if (primary === "vaca") {
+    secondaryOptions = [{ label: "NOVILHA", value: "novilha" }];
+  } else if (
+    primary === "touro" ||
+    primary === "vaca invernar" ||
+    primary === null
+  ) {
+    //"touro" ou "vaca invernar" ou placeholder
+    secondaryOptions = [];
+  }
+
+  const handleSelection = (value) => {
+    setPrimary(value);
+    setSecondary("");
+    if (value === "touro" || value === "vaca invernar" || value === null) {
+      setSecondaryToggle(false);
+    } else {
+      setSecondaryToggle(true);
+    }
+  };
+
   return (
     <Container>
       {/* Video upload */}
@@ -28,43 +91,40 @@ export default function AnnouncementCreation(props) {
         </UploadVideo>
       </TouchableOpacity>
 
-      {/*
+      {/* Primary Category Selection */}
+      <Text>Selecione uma categoria</Text>
       <RNPickerSelect
-        placeholder={"Selecione uma categoria"}
-        items={sports}
-        onValueChange={(value) => {
-          this.setState({
-            favSport0: value,
-          });
+        placeholder={{
+          label: "———",
+          value: null,
+          color: general.styles.colors.light,
         }}
-        onUpArrow={() => {
-          this.inputRefs.firstTextInput.focus();
-        }}
-        onDownArrow={() => {
-          this.inputRefs.favSport1.togglePicker();
-        }}
-        style={pickerSelectStyles}
-        value={this.state.favSport0}
-        ref={(el) => {
-          this.inputRefs.favSport0 = el;
-        }}
-      /> */}
-
-      <RNPickerSelect
-        onValueChange={(value) => console.log("S-Category2: ", value)}
-        items={[
-          { label: "Football", value: "football" },
-          { label: "Baseball", value: "baseball" },
-          { label: "Hockey", value: "hockey" },
-        ]}
+        onValueChange={(value) => handleSelection(value)}
+        items={primaryOptions}
       />
+
+      {/* Secondary Category Selection */}
+      {secondaryToggle ? (
+        <>
+          <Text>Selecione, se houver, outra categoria</Text>
+          <RNPickerSelect
+            placeholder={{
+              label: "———",
+              value: null,
+              color: general.styles.colors.light,
+            }}
+            onValueChange={(value) => setSecondary(value)}
+            items={secondaryOptions}
+          />
+        </>
+      ) : null}
     </Container>
   );
 }
 
 AnnouncementCreation.navigationOptions = ({ navigation }) => {
   return {
-    title: "Criar um anúncio",
+    title: "Crie um anúncio",
     headerBackTitleVisible: false,
   };
 };
