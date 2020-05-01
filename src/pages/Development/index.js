c4d5c5540f5606476221e400ceda900216cb3c49
JM Fantin <joao.fantin@acad.pucrs.br>
2020-05-01 17:55:14 -0300

    Dev page

--- src/pages/Development/index.js
+++ src/pages/Development/index.js
@@ -1,31 +1,31 @@
-// import React from "react";
-// import { Text } from "react-native";
-// import PropTypes from "prop-types";
+import React from "react";
+import { Text } from "react-native";
+import PropTypes from "prop-types";
 
-// import { Container } from "./styles";
+import { Container } from "./styles";
 
 // import Monetary from "./components/Monetary";
 
-// export default function Development() {
-//   return (
-//     <Container>
-//       <Text>
-//         Instancie componentes abaixo{"\n"}Não trate lógica nessa página
-//       </Text>
-//       <Monetary />
-//     </Container>
-//   );
-// }
+export default function Development() {
+  return (
+    <Container>
+      <Text>
+        Instancie componentes abaixo{"\n"}Não trate lógica nessa página
+      </Text>
+      {/* <Monetary /> */}
+    </Container>
+  );
+}
 
-// Development.navigationOptions = ({ navigation }) => {
-//   return {
-//     title: "Teste de componentes",
-//     headerBackTitleVisible: false,
-//   };
-// };
+Development.navigationOptions = ({ navigation }) => {
+  return {
+    title: "Teste de componentes",
+    headerBackTitleVisible: false,
+  };
+};
 
-// Development.propTypes = {
-//   navigation: PropTypes.shape({
-//     dispatch: PropTypes.func,
-//   }).isRequired,
-// };
+Development.propTypes = {
+  navigation: PropTypes.shape({
+    dispatch: PropTypes.func,
+  }).isRequired,
+};
