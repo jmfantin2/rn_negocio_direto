aa9cdc83ba3841e5c9a6c8ac92f4ddc9f57cb768
Gabriel Brunichaki <6abriel.dev@gmail.com>
2020-07-15 23:16:33 -0300

    data from api

--- src/pages/AnnouncementList/index.js
+++ src/pages/AnnouncementList/index.js
@@ -1,31 +1,61 @@
-import React from 'react';
-import { Text, View } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { FlatList, Text, View } from 'react-native';
 import { Button, Card, Chip, Paragraph, Title } from 'react-native-paper';
 
+import api from '../../services/ann';
+
 const AnnouncementList = () => {
+  const [announcements, setAnnouncements] = useState([]);
+
+  useEffect(() => {
+    async function fetchAnnouncements() {
+      const response = await api.get('/api/v1/announcements');
+
+      setAnnouncements(response.data.content);
+    }
+
+    fetchAnnouncements();
+  }, []);
+
   return (
-    <View style={{ margin: 16 }}>
-      <Card style={{ width: 200 }}>
-        <Card.Cover
-          style={{ height: 120, margin: 12, borderRadius: 5 }}
-          source={{
-            uri:
-              'https://www.peta.org/wp-content/uploads/2017/07/iStock-502605347_emholk-1-668x336-1564757931.jpg?20190802025851',
-          }}
-        />
-        <Card.Content>
-          <View style={{ marginBottom: 8, flexDirection: 'row' }}>
-            <Chip style={{ marginRight: 8 }}>Terneiros</Chip>
-            <Chip>Vacas</Chip>
-          </View>
-          <Paragraph style={{ color: '#b5b5b5' }}>62 cabeças</Paragraph>
-          <Title>R$ 650,00</Title>
-          <Text style={{ color: '#6B9061', fontSize: 16, marginTop: 8 }}>
-            Porto Alegre (RS)
-          </Text>
-        </Card.Content>
-      </Card>
-    </View>
+    <FlatList
+      data={announcements}
+      numColumns={2}
+      keyExtractor={(item) => item.id}
+      renderItem={({ item }) => (
+        <Card style={{ width: 180 }}>
+          <Card.Cover
+            style={{ height: 120, margin: 12, borderRadius: 5 }}
+            source={{
+              uri:
+                'https://www.peta.org/wp-content/uploads/2017/07/iStock-502605347_emholk-1-668x336-1564757931.jpg?20190802025851',
+            }}
+          />
+          <Card.Content>
+            <View
+              style={{
+                marginBottom: 8,
+                flexDirection: 'row',
+                flexWrap: 'wrap',
+              }}
+            >
+              {item.category.map(({ name }) => (
+                <Chip key={name} style={{ marginRight: 8, marginBottom: 8 }}>
+                  {name}
+                </Chip>
+              ))}
+            </View>
+            <Paragraph
+              style={{ color: '#b5b5b5' }}
+            >{`${item.animalsQuantity} cabeças`}</Paragraph>
+            <Title>{`R$ ${item.currentPrice},00`}</Title>
+            <Text style={{ color: '#6B9061', fontSize: 16, marginTop: 8 }}>
+              {`${item.location.city} (${item.location.state})`}
+            </Text>
+          </Card.Content>
+        </Card>
+      )}
+    />
   );
 };
 
